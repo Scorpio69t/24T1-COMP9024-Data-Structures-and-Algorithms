b6a3fba0e33004136a6d406a1dce0dedf2cdf731
Kevin Nguyen <kevinngx625@gmail.com>
2024-03-12 09:22:00 +1100

    Lecture Notes

---
+++ W04 Graph Data Structures/lecture_notes.c
@@ -0,0 +1,12 @@
+// insertEdge(g, (v,w)):
+//     Input graph g, edge (v,w)
+// 
+//     g.edges[g.nE] = (v,w);
+//     g.nE = g.nE + 1;
+
+// removeEdge(g, (v,w)):
+// while (v,w) != g.edhes[i] do
+//     i++;
+// end while
+// g.edges[i] = g.edges[g.nE-1]
+// g.nE = g.nE -1
