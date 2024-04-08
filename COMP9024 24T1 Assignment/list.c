cea472d92ccb8635375633f701cb54028d10ec66
Kevin Nguyen <kevinngx625@gmail.com>
2024-04-08 21:25:15 +1000

    W08 Practical

--- COMP9024 24T1 Assignment/list.c
+++ COMP9024 24T1 Assignment/list.c
@@ -1,28 +1,5 @@
 // kevinngx - z5114919@ad.unsw.edu.au
 
-// You have been provided with a file list.h. Examine the file carefully. It provides the interface for an ADT that will provide Queue, Stack, and Set functionality.
-
-// Your task is to implement the functions prototyped in the list.h header file within list.c.
-
-// You must create the file list.c to implement this ADT.
-// You must store string (char *) data within the ADT.
-// You must allocate memory dynamically.
-// You must not modify the list.h file.
-// You must not modify the function prototypes declared in the list.h file.
-// You may add utility functions to the list.c file.
-// You may use the string.h library, and other standard libraries from the weekly exercises.
-// You may reuse code previously submitted for weekly assessments and provided in the lectures.
-// You may use whatever internal representation you like for your list ADT, provided it does not contradict any of the above.
-// You may assume that any instance of your list ADT will only be used as a queue or a stack or a set.
-// You should write programs that use your ADT to test and debug your code.
-// You should use valgrind to verify that your ADT does not leak memory.
-// As a reminder:
-
-// Queue - First In, First Out
-// Stack - First In, Last Out
-// Set - Only stores unique values.
-// See list.h for more information about each function that you are required to implement.
-
 #include "list.h"
 #include "string.h"
 #include <assert.h>
@@ -94,6 +71,7 @@ size_t list_length(list l)
 }
 
 /****************************** STACK INTERFACE *******************************/
+
 void list_push(list l, string s)
 {
     if (l == NULL || s == NULL)
@@ -137,6 +115,7 @@ string list_pop(list l)
 }
 
 /****************************** QUEUE INTERFACE *******************************/
+
 void list_enqueue(list l, string s)
 {
     if (l == NULL || s == NULL)
@@ -196,6 +175,7 @@ string list_dequeue(list l)
 }
 
 /******************************* SET INTERFACE ********************************/
+
 void list_add(list l, string s)
 {
     if (!list_contains(l, s))
---
+++ W08 Search Tree Data Structures/lecture.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Running program\n");
+    return 0;
+}
